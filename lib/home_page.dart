a31518e94ca53a1ec935fc09573e425912117788
KritMukul <mukulkrit14@gmail.com>
2024-08-24 22:36:48 +0530

    Centered Home-Page Text

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,38 +9,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(context) {
     return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            'assets/images/quiz-logo.png',
-            width: 300,
-            color: const Color.fromARGB(151, 255, 255, 255),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Text(
-            'Learn Flutter the fun Way!',
-            style: GoogleFonts.ubuntu(
-                color: const Color.fromARGB(255, 239, 197, 254), fontSize: 30),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          OutlinedButton.icon(
-            style: OutlinedButton.styleFrom(
-              foregroundColor: const Color.fromARGB(255, 240, 203, 250),
-              shape: const LinearBorder(),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(151, 255, 255, 255),
             ),
-            onPressed: startQuiz,
-            icon: const Icon(Icons.arrow_right_alt),
-            label: const Text(
-              'Start Quiz',
-              style: TextStyle(fontSize: 10),
+            const SizedBox(
+              height: 50,
             ),
-          ),
-        ],
+            Text(
+              'Learn Flutter the fun Way!',
+              style: GoogleFonts.ubuntu(
+                  color: const Color.fromARGB(255, 239, 197, 254),
+                  fontSize: 25),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 240, 203, 250),
+                shape: const LinearBorder(),
+              ),
+              onPressed: startQuiz,
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text(
+                'Start Quiz',
+                style: TextStyle(fontSize: 10),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
